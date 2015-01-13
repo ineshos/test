19dc3871c8434573e06f023a17c39c4a670bccdd
ineshos <ines.hosni@hotmail.fr>
2015-01-13 11:30:42 -0200

    commit2

---
+++ HELLOWORD/hello.h
@@ -0,0 +1,17 @@
+/*
+ * hello.h
+ *
+ *  Created on: 13 janv. 2015
+ *      Author: samsung
+ */
+
+#ifndef HELLO_H_
+#define HELLO_H_
+
+class hello {
+public:
+	hello();
+	virtual ~hello();
+};
+
+#endif /* HELLO_H_ */
